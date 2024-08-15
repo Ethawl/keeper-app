d8f2f2d1445bd994de2feb528c0354e8c1b848ac
Ethawl <161845425+Ethawl@users.noreply.github.com>
2024-08-15 21:21:38 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -118,6 +118,14 @@ app.post('/login', async (req, res) => {
 });
 
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
